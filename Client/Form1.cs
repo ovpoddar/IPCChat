dd24ed7f2d1ddea4ac9eb4b5916f918cc67e5d6a
Ayan Poddar <ayanpoddar9@gmail.com>
2021-06-24 03:25:08 -0700

    Add most basic form of IPC

---
+++ Client/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            if (!socket.Connected)
+                socket.Connect(IPAddress.Parse("127.0.0.1"), 1000);
+            socket.Send(Encoding.ASCII.GetBytes("hlw"));
+            socket.Dispose();
+        }
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Socket Server { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Server.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1000));
+            Server.Listen(10);
+            Server.BeginAccept(Acceptstart, null);
+        }
+        private void Acceptstart(IAsyncResult ar)
+        {
+            var user = Server.EndAccept(ar);
+            var buffer = new byte[user.ReceiveBufferSize];
+            user.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, senddata, null);
+
+            void senddata(IAsyncResult ar)
+            {
+                var received = user.EndReceive(ar);
+                if (received == 0)
+                    return;
+                var message = Encoding.ASCII.GetString(buffer, 0, received);
+                Invoke((Action)delegate
+                {
+                    listBox1.Items.Add(message);
+                });
+                user.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, senddata, null);
+            }
+            Server.BeginAccept(Acceptstart, null);
+        }
+    }
+}
